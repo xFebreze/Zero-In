777b2c1b05e1214df34d7953d2a505b1588ce84c
voyatz <97427081+voyatz@users.noreply.github.com>
2022-01-31 08:00:37 -0500

    Delete archive directory

--- archive/absolutewrite.py
+++
@@ -1,9 +0,0 @@
-with open('manifest.json', 'r') as file:
-    manifest = file.readlines()
-
-blockedURLs = "https://*.nytimes.com/*", "https://*.tiktok.com/*"
-
-manifest[22] = "     \"matches\": [\"" + "\",\"".join(blockedURLs) + "\"],"
-  
-with open('manifest.json', 'w') as file:
-    file.writelines(manifest)
--- archive/alek.js
+++
@@ -1,3 +0,0 @@
-if(window.location.hostname === "www.youtube.com"){
-  alert("Cringe!");
-}
