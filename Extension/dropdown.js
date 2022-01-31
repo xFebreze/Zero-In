02de229aa217f59551e4712baa06a7fee888a8c1
voyatz <97427081+voyatz@users.noreply.github.com>
2022-01-31 10:34:19 -0500

    simplified code

--- Extension/dropdown.js
+++ Extension/dropdown.js
@@ -1,21 +1,9 @@
 window.onload = function() {
-  console.log("onload" + Date())
   chrome.storage.sync.get('productivityOn',(value)=>{
 
-    var button = document.getElementById('PM_button');
-
-    if (value.productivityOn == true){
-
-      button.style.backgroundColor = '#e09320';
-      console.log("Zero-In: Productivity Mode turned on!");
-
-    }
-
-    else{
-
+    if (value.productivityOn == false){
+      var button = document.getElementById('PM_button');
       button.style.backgroundColor = '#b0741a';
-      console.log("Zero-In: Productivity Mode turned off!");
-
     }
 
   })
