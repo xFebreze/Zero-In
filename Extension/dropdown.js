1ebdffd9f2639f599953fed9f60c337ec574c4da
voyatz <vvoyatzoglou@ufl.edu>
2022-02-02 12:37:36 -0500

    added instant block/unblock feedback

--- Extension/dropdown.js
+++ Extension/dropdown.js
@@ -1,3 +1,41 @@
+popUp = `<html>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;700&family=Roboto+Mono:wght@300;700&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+        <style>
+
+            body{
+              text-align: center;
+              font-family: 'Open Sans', sans-serif;
+              color: white;
+            }
+
+            h1{
+              font-family: 'Roboto Mono', monospace;
+              font-size: 60px;
+            }
+
+            .center{
+                length: 500;
+                width: 500;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+        </style>
+        <body style="background-image: linear-gradient(to bottom right, #B9EFA3, #008693)">
+          <h1>Nonono...</h1>
+            <img src="https://cdn.discordapp.com/attachments/929849977423929407/937767116759789568/nocat4.png" al></img>
+          <p>get back to work!</p>
+            <marquee behavior="slide"scrollamount="40"><img src="https://cdn.discordapp.com/attachments/929849977423929407/937161808316366868/no-cat.gif"class="center"></img></marquee>
+        </body>
+    </head>
+</html>
+`
+
 function format(uncutUrl){
 
   var posMid = (uncutUrl.indexOf('.'));
@@ -59,11 +97,17 @@ BW_button.onclick = () => {
         newList.push(url);
         console.log("Zero-In: " + url + " added to blacklist!")
         BW_button.innerHTML = 'Unblock Website';
+        chrome.tabs.reload(tabs[0].id);
       }
       else{
         newList.splice(index, 1);
         console.log("Zero-In: " + url + " removed from the blacklist!");
         BW_button.innerHTML = 'Block Website';
+        chrome.storage.sync.get('productivityOn',(value)=>{
+          if (value.productivityOn == true){
+            chrome.tabs.reload(tabs[0].id);
+          }
+        })
       }
 
       chrome.storage.sync.set({'blacklist': newList},()=>{
